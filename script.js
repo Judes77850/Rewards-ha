de2c8d7e89b6868382b88800878cd911c7447b02
Julien Desaindes <julien.desaindes@gmail.com>
2022-11-14 10:45:34 +0100

    ajustement

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-// document.getElementById("onClick").addEventListener("click", function () {
-//   document.body.style.backgroundColor = "white";
-// });
-
-function myFunctionDesktop() {
+async function start() {
   for (let i = 50; i < 81; i++) {
     let button = document.getElementById("onClick1");
     let link =
       "https://www.bing.com/search?q=bien-joué" +
       [i] +
       "&form=QBLH&sp=-1&pq=test&sc=10-4&qs=n&sk=&cvid=C18421E39EB741F7AF6EA71C4439BFFB&ghsh=0&ghacc=0&ghpl=";
-    window.open(link);
+    await openLink(link);
     button.style.backgroundColor = "green";
   }
 }
 
-function myFunctionMobile() {
-  for (let i = 0; i < 22; i++) {
-    let button = document.getElementById("onClick2");
+async function start2() {
+  for (let i = 0; i < 21; i++) {
+    let button = document.getElementById("onClick1");
     let link =
-      "https://www.bing.com/search?q=coucou" +
+      "https://www.bing.com/search?q=bien-joué" +
       [i] +
       "&form=QBLH&sp=-1&pq=test&sc=10-4&qs=n&sk=&cvid=C18421E39EB741F7AF6EA71C4439BFFB&ghsh=0&ghacc=0&ghpl=";
-    window.open(link);
+    await openLink(link);
     button.style.backgroundColor = "green";
   }
+}
+
+async function openLink(url) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      window.open(url, "ABC");
+      resolve();
+    }, 1000);
+  });
 }
