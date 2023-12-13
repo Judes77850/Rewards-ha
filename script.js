7e346c914cc224121cebd3016c6973b1fe5a3dcf
Julien <julien.desaindes@gmail.com>
2023-12-13 14:40:25 +0100

    change search

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function start() {
   for (let i = 50; i < 93; i++) {
     let button = document.getElementById("onClick1");
     let link =
-      "https://www.bing.com/search?q=bien-joué" +
+      "https://www.bing.com/search?q=Arme-fatale" +
       [i] +
       "&form=QBLH&sp=-1&pq=test&sc=10-4&qs=n&sk=&cvid=C18421E39EB741F7AF6EA71C4439BFFB&ghsh=0&ghacc=0&ghpl=";
     await openLink(link);
@@ -14,7 +14,7 @@ async function start2() {
   for (let i = 0; i < 33; i++) {
     let button = document.getElementById("onClick1");
     let link =
-      "https://www.bing.com/search?q=bien-joué" +
+      "https://www.bing.com/search?q=Arme-fatale" +
       [i] +
       "&form=QBLH&sp=-1&pq=test&sc=10-4&qs=n&sk=&cvid=C18421E39EB741F7AF6EA71C4439BFFB&ghsh=0&ghacc=0&ghpl=";
     await openLink(link);
